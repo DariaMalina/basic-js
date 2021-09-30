902c9f2bbfe1aa61af59a85cc74b1c66e0eaf748
Daria Malina <gluto68@gmail.com>
2021-10-01 00:56:26 +0300

    feat: add solution task

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +10,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let string = "";
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      if (counter === 1) {
+        string += str[i];
+      } else {
+        string += counter + str[i];
+        counter = 1;
+      }
+    } else {
+      counter++;
+    }
+  }
+  return string;
 }
