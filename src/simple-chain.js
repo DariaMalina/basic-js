e5d208cb9bdb211582c44969f17331295f5d40ba
Daria Malina <gluto68@gmail.com>
2021-10-02 01:56:56 +0300

    feat: add solution task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,40 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+  array: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.array.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position > this.array.length || position <= 0 || !Number(position)) {
+      this.array = [];
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.array.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let finaString = "";
+    for (let i = 0; i < this.array.length; i++) {
+      if (i === this.array.length - 1) {
+        finaString += `( ${this.array[i]} )`;
+      } else {
+        finaString += `( ${this.array[i]} )`;
+        finaString += `~~`;
+      }
+    }
+    this.array = [];
+    return finaString;
+  },
 };
