a1a33e723fa97e9bde1b0149d20919fb8501f025
Daria Malina <gluto68@gmail.com>
2021-10-04 23:11:13 +0300

    feat: add solution task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,15 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(sampleActivity) {
-  if (!Number(sampleActivity)) {
-    return false;
+  if (
+    typeof sampleActivity === "string" &&
+    Number(sampleActivity) > 0 &&
+    Number(sampleActivity) <= 15
+  ) {
+    return Math.ceil(
+      Math.log(MODERN_ACTIVITY / Number(sampleActivity)) /
+        (0.693 / HALF_LIFE_PERIOD)
+    );
   }
+  return false;
 }
